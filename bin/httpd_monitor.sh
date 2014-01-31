013067731575ff3621a05d90575962b2ed0cb98a
James Whiteneck <james.whiteneck@rentrak.com>
2014-01-31 09:28:44 -0800

    Changed speedy to speedy_backend in httpd monitoring script.

--- bin/httpd_monitor.sh
+++ bin/httpd_monitor.sh
@@ -1,1 +1,1 @@
-watch -n 5 'echo "$(hostname) - HTTPD: $(ps -ef | grep httpd | wc -l)   Speedy: $(ps -ef | grep speedy | wc -l)"'
+watch -n 5 'echo "$(hostname) - HTTPD: $(ps -ef | grep httpd | wc -l)   Speedy: $(ps -ef | grep speedy_backend | wc -l)"'
